fc18837755f96eaba57c0e84c1e26df4d0ed1d98
Julian Perez <julianperez.developer@gmail.com>
2020-12-19 12:30:42 -0500

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import { firestore } from "./firebase";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Burrito = () => {
+  const [selectValue, setselectValue] = useState('');
+  const [historyOffers, sethistoryOffers] = useState({ offers: {} });
+  const [message, setmessage] = useState();
+
+  useEffect(() => {
+    firestore
+      .collection("HistoryOffersNotificationCenter")
+      .get()
+      .then(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => { return { ...doc.data(), id: doc.id }; });
+        console.log(data);
+        sethistoryOffers({ offers: data });
+      });
+
+  }, []);
+
+  const handleSubmit = (e) => {
+    setmessage();
+    e.preventDefault();
+
+    if (selectValue !== '') {
+      const splitValue = selectValue.split('|');
+      firestore.collection("ConfirmationPayments").doc(`offer_${selectValue}`).set({
+        offerId: splitValue[0],
+        date: new Date().toLocaleString(),
+        userId: splitValue[1],
+        status: "RESUME",
+      });
+
+      setmessage({ text: '¡Se ha validado el pago exitosamente!', type: 'success' });
+    } else setmessage({ text: '¡Ocurrió un problema, intentalo nuevamente!', type: 'warning' });
+
+  };
+
+  const handleChange = (value) => {
+    setselectValue(value.target.value);
+  };
+
+  return historyOffers.offers.length > 0 ? (
+    <div className="main-container">
+      {message && (
+        <div className={`message ${message.type}`}>{message.text}</div>
+      )}
+      <form className="form-container" method="POST" onSubmit={handleSubmit}>
+        <label className="label-container">
+          {`Selecciona la oferta a realizar validación de pago: `}
+          <select className="select-container" value={selectValue} onChange={handleChange}>
+            {historyOffers.offers.map((offer) => (
+              <option className="select-option" value={`${offer.id}|${offer.userId}`}>{`Origen: ${offer.currentCity} - destino: ${offer.destinationCity} - offerId: ${offer.id} - valor: ${offer.offerValue} `}</option>
+            ))}
+          </select>
+        </label>
+        <div className="btn-container">
+          <input className="btn" type="submit" value="Cargar" />
+        </div>
+      </form>
     </div>
-  );
+  ) : <p>Cargando...</p>;
 }
 
-export default App;
+export default Burrito;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from'firebase';
+import 'firebase/firestore';
+
+const config =  {
+  apiKey: "AIzaSyAx3ZM1YpfTSiV4dennpgT3hiZcJ2959s8",
+  authDomain: "cargamedb-p1.firebaseapp.com",
+  databaseURL: "https://cargamedb-p1.firebaseio.com",
+  projectId: "cargamedb-p1",
+  storageBucket: "cargamedb-p1.appspot.com",
+  messagingSenderId: "950532818245",
+  appId: "1:950532818245:web:f583866904f66124bd2a2c",
+  measurementId: "G-WJ6SMF3D9Y"
+};
+
+firebase.initializeApp(config);
+
+export const firestore = firebase.firestore();
+
+export default firebase;
